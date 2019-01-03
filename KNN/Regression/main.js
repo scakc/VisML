df1f1eab25426c3bf0ac59fccb4fe60ddac4e593
scakc <ak23@iitbbs.ac.in>
2019-01-03 23:10:59 +0530

    KNN regression init changes

--- KNN/Regression/main.js
+++ KNN/Regression/main.js
@@ -172,7 +172,7 @@ function grid1dflat(xmin,xmax,nX){
 var cmap = ['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf']
 var nClasses = 1;
 var nSamples = 50;
-var seed = 5;
+var seed = 2;
 var datas,cmeans,cvals,xmin,xmax,ymin,ymax, grid;
 
 cINIT(nClasses, nSamples,seed);
