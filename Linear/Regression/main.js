a8f8c1d9e8124f5c8635c6e6d376c8c281d1f967
scakc <ak23@iitbbs.ac.in>
2019-01-07 21:25:53 +0530

    Linear Regression

---
+++ Linear/Regression/main.js
@@ -0,0 +1,170 @@
+function init(nClasses, nSamples, seed){
+    var means = tf.randomNormal([nClasses*2,1], 0, 4, 'float32', seed) // 2C x 1
+    var stds = tf.add(tf.randomUniform([nClasses*2,1], 0 , 0.5,'float32', seed), 1) // 2C x 1
+
+    var k, meanx, meany, stdx, stdy, datax, datay,data1, trace, datas, cmeans, cvals, pdata;
+    
+    for (var i = 0; i < nClasses; i++) {
+        k = 2*i;
+        meanx = means.dataSync()[k]
+        meany = means.dataSync()[k+1]
+        stdx = stds.dataSync()[k]
+        stdy = stds.dataSync()[k+1]
+
+        datax = tf.randomNormal([nSamples,1], meanx, stdx); // S x 1
+        datay = tf.add(datax ,tf.randomNormal(datax.shape,0, 0.15));
+
+        data1 = tf.concat([datax, datay], axis = 1); // S x 2
+
+        trace = {
+        x: Array.from(data1.slice([0, 0],[nSamples,1]).dataSync()),
+        y: Array.from(data1.slice([0, 1],[nSamples,1]).dataSync()),
+        mode : 'markers',
+        type : 'scatter',
+        name : 'class ' + i,
+        marker :{
+            size :6,
+        }
+        };
+
+
+        if(i==0){
+            datas = data1;
+            cmeans = data1.mean(axis=0).reshape([1,2]); // 1 x 2
+            cvals = tf.mul(tf.ones([data1.shape[0], 1]), i);
+            pdata = [trace]
+        }else{
+            datas = tf.concat([datas,data1], axis = 0); // SC x 2 finally
+            cmeans = tf.concat([cmeans, data1.mean(axis= 0).reshape([1,2])], axis = 0); // C x 2
+            cvals = tf.concat([cvals, tf.mul(tf.ones([data1.shape[0], 1]), i)], axis = 0); // CS x 1
+            pdata = pdata.concat(trace);
+        }
+
+    }
+
+
+
+
+    layout = { 
+      title: "Visualization",
+      font: {size: 18}
+    };
+
+
+    
+
+    Plotly.newPlot('myDiv', pdata, layout, {responsive: true});
+
+    return [datas, cmeans, cvals, pdata];
+
+}
+
+function process(){
+
+    var N, k , mx, my, mean, dist, sdist, labels, trace, layout, kval;
+
+    kval = parseInt(document.getElementById("kval").value);
+
+    
+    CS = datas.shape[0];
+
+    var r_vals = datas.slice([0,1], [CS,1]).reshape([-1]); // X
+    var r_data = datas.slice([0,0], [CS,1]).reshape([-1]); // Y
+    var r_grid = grid;
+    
+    N = r_grid.shape[0];
+
+    var X = r_data.reshape([-1,1]);
+    var Y = r_vals.reshape([-1,1]);
+    var Xstr = r_grid.reshape([-1,1]);
+    var D = X.shape[0];
+
+    var inv_xtx = tf.div(1,tf.dot(tf.transpose(X), X));
+    var xty = tf.dot(tf.transpose(X), Y)
+    var W = tf.dot(inv_xtx, xty);
+
+    labels = tf.mul(W, Xstr);
+
+    trace = {
+    x: Array.from(grid.dataSync()),
+    y: Array.from(labels.dataSync())
+    };
+
+
+    layout = { 
+      title: "Visualizing Regression",
+      font: {size: 18}
+    };
+
+    cData = pdata.concat(trace);
+
+    Plotly.newPlot('myDiv', cData, layout, {responsive: true});
+
+
+}
+
+
+
+document.getElementById("init").onclick = function() {varys = cINIT()
+    nSamples = varys[1];
+    nClasses = varys[0];
+    seed = varys[2];
+};
+document.getElementById("cfy").onclick = function() {process()};
+
+
+function cINIT(nClasses, nSamples, seed) {
+    if(nClasses){
+        ;
+    }else{
+        nClasses = 1;
+        nSamples = document.getElementById("nSamples").value;
+        seed = document.getElementById("seed").value;
+
+        nSamples = parseInt(nSamples);
+        nClasses = parseInt(nClasses);
+        seed = parseInt(seed);
+
+    }
+    cache = init(nClasses, nSamples, seed);
+
+    datas = cache[0];
+    cmeans = cache[1];
+    cvals = cache[2];
+    pdata = cache[3];
+
+    // alert(nSamples);
+
+    xymin = tf.min(datas, axis = 0).dataSync();
+    xymax = tf.max(datas, axis = 0).dataSync();
+
+    xmin = xymin[0];
+    ymin = xymin[1];
+    xmax = xymax[0];
+    ymax = xymax[1];
+
+    grid = grid1dflat(xmin, xmax, 100);
+
+    return [nClasses, nSamples, seed];
+}
+
+function cADD() {
+    
+}
+
+function grid1dflat(xmin,xmax,nX){
+    var xsteps = (xmax - xmin)/nX;
+    var xvals = tf.range(xmin,xmax,xsteps).reshape([-1,1]);
+    return xvals;
+    //
+}
+
+
+var cmap = ['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf']
+var nClasses = 1;
+var nSamples = 50;
+var seed = 2;
+var datas,cmeans,cvals,xmin,xmax,ymin,ymax, grid;
+
+cINIT(nClasses, nSamples,seed);
+
--- utils.js
+++ utils.js
@@ -18,3 +18,83 @@ function grid1dflat(xmin,xmax,nX){
     return xvals;
     //
 }
+
+
+// calculate the determinant of a matrix m
+function det(m) {
+    return tf.tidy(() => {
+        const [r, _] = m.shape
+        if (r === 2) {
+            const t = m.as1D()
+            const a = t.slice([0], [1]).dataSync()[0]
+            const b = t.slice([1], [1]).dataSync()[0]
+            const c = t.slice([2], [1]).dataSync()[0]
+            const d = t.slice([3], [1]).dataSync()[0]
+            result = a * d - b * c
+            return result
+
+        } else {
+            let s = 0;
+            rows = [...Array(r).keys()]
+            for (let i = 0; i < r; i++) {
+                sub_m = m.gather(tf.tensor1d(rows.filter(e => e !== i), 'int32'))
+                sli = sub_m.slice([0, 1], [r - 1, r - 1])
+                s += Math.pow(-1, i) * det(sli)
+            }
+            return s
+        }
+    })
+}
+
+// the inverse of the matrix : jordan-gauss method
+function invertM(m) {
+    return tf.tidy(() => {
+        if (det(m) === 0) {
+            console.log('Zero det')
+            return
+        }
+        const [r, _] = m.shape
+        let inv = m.concat(tf.eye(r), 1)
+        rows = [...Array(r).keys()]
+        for (let i = 0; i < r; i++) {
+            inv = tf.tidy(() => {
+                for (let j = i + 1; j < r; j++) {
+                    const elt = inv.slice([j, i], [1, 1]).as1D().asScalar()
+                    const pivot = inv.slice([i, i], [1, 1]).as1D().asScalar()
+                    let newrow
+                    if (elt.dataSync()[0] !== 0) {
+                        newrow = inv.gather(tf.tensor1d([i], 'int32')).sub(inv.gather(tf.tensor1d([j], 'int32')).div(elt).mul(pivot)).as1D()
+                        const sli = inv.gather(rows.filter(e => e !== j))
+                        const arr = []
+                        if (j === 0) {
+                            arr.push(newrow)
+                        }
+                        sli.unstack().forEach((t, ind) => {
+                            if (ind !== j) {
+                                arr.push(t)
+                            } else {
+                                arr.push(newrow)
+                                arr.push(t)
+                            }
+                        })
+                        if (j === r - 1) {
+                            arr.push(newrow)
+                        }
+                        inv = tf.stack(arr)
+                    }
+                }
+                return inv
+            })
+        }
+        const trian = tf.unstack(inv)
+        len = trian.length
+        trian[len - 1] = trian[len - 1].div(trian[len - 1].slice(trian[len - 1].shape[0] - 1, 1).asScalar())
+        for (let i = r - 2; i > -1; i--) {
+            for (j = r - 1; j > i; j--) {
+                trian[i] = trian[i].sub(trian[j].mul(trian[i].slice(j, 1).asScalar()))
+            }
+            trian[i] = trian[i].div(trian[i].slice(i, 1).asScalar())
+        }
+        return tf.split(tf.stack(trian), 2, 1)[1]
+    })
+}
